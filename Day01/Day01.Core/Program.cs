fd32638f3a29f5a19f25f4852e6aad23f7d3c713
Saw Naw <waterchan@gmail.com>
2022-12-07 20:24:34 +0000

    Add Program.cs

--- Day01/Day01.Core/Program.cs
+++ Day01/Day01.Core/Program.cs
@@ -6,4 +6,9 @@ var file = new FileParser(@"input.txt");
 var result = file.Parse();
 
 // Put a breakpoint here and inspect the contents of the result object.
-Console.WriteLine("Done!");
+Console.WriteLine("Top 3 elves carrying the most calories are:");
+foreach (var item in result.CalorieAndPositions)
+{
+    Console.WriteLine($"  Elf at Position {item.Position}, carrying {item.HighestCalorie} calories");
+}
+Console.WriteLine($"In total, these elves are carrying {result.CalorieAndPositions.Sum(c => c.HighestCalorie)} calories.");
