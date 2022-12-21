f046eef7ddedaf076228521295ed6eadc08b3578
Saw Naw <waterchan@gmail.com>
2022-12-21 17:43:28 +0000

    Fix file structure

---
+++ Day07/Day07.Filesystem/FileSystem.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day07.Filesystem
+{
+    internal class FileSystem
+    {
+        private List<File> _content = new();
+        public IReadOnlyCollection<File> Content => _content;
+
+        public void ExecuteCommand(Command cmd)
+        {
+
+        }
+
+        private void ExecuteLsCommand()
+        {
+
+        }
+
+        private void ExecuteCdCommand()
+        {
+
+        }
+    }
+}
