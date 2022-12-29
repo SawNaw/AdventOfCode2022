8de3ba33a30ce5c36fbf7fc22c7c4e75585aae8a
Saw Naw <waterchan@gmail.com>
2022-12-29 22:59:21 +0000

    day 8 part 1 done

--- Day08/Day08.Core/Program.cs
+++ Day08/Day08.Core/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using Day08.Trees;
+
+Console.WriteLine("Advent Of Code -- Day 08");
+Console.WriteLine("");
+
+var forest = new Forest(@"input.txt");
+
+Console.WriteLine("Answer to Part One");
+Console.WriteLine("--------------------------");
+Console.WriteLine($"The answer is: {forest.GetNumberOfVisibleTrees()}");
+
+Console.WriteLine("");
+
+
+Console.WriteLine("Answer to Part Two");
+Console.WriteLine("--------------------------");
+Console.WriteLine($"The answer is: ");
