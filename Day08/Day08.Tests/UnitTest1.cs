aec29751f053cc4ae46078e88d8c967bbf7b2b02
Saw Naw <waterchan@gmail.com>
2022-12-29 13:02:34 +0000

    Day 08 initial

---
+++ Day08/Day08.Tests/UnitTest1.cs
@@ -0,0 +1,11 @@
+namespace Day08.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+
+        }
+    }
+}
---
+++ Day08/Day08.Trees/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace Trees
+{
+    public class Class1
+    {
+
+    }
+}
