d54cdd4d70d409770d3da9f3e5fa30ec11fcb66d
Saw Naw <waterchan@gmail.com>
2022-12-31 11:32:17 +0000

    Cleanup

--- Day09/Day09.RopeMotionSimulator/Coordinate.cs
+++ Day09/Day09.RopeMotionSimulator/Coordinate.cs
@@ -1,12 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.CompilerServices;
-using System.Security.Cryptography.X509Certificates;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace Day09.RopeMotionSimulator
+﻿namespace Day09.RopeMotionSimulator
 {
     internal struct Coordinate
     {
